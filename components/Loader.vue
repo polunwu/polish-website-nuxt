79e25fe8842bd20548acbce8d95311f97757afcd
polunwu <polunwu@gmail.com>
2020-07-20 15:40:40 +0800

    Implementing layout mounted animation with Loader component

---
+++ components/Loader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="loader">
+    <img
+      class="loader__logo"
+      src="~/assets/images/header_logo.svg"
+      alt="Polish Design Logo"
+    />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.loader {
+  position: fixed;
+  background: $primary-color;
+  width: 100vw;
+  height: 100vh;
+  z-index: 999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+  img {
+    width: 140px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <transition name="fade">
+      <Loader v-show="isLoading" />
+    </transition>
     <Navbar />
     <Nuxt />
     <Footer />
@@ -7,17 +10,56 @@
 </template>
 
 <script>
+import Loader from '@/components/Loader.vue'
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
+    Loader,
     Navbar,
     Footer,
+  },
+  data() {
+    return {
+      isLoading: true,
+    }
+  },
+  mounted() {
+    this.isLoading = false
   },
 }
 </script>
 
 <style lang="scss">
 @import '~/assets/scss/_general.scss';
+
+.fade-enter-active {
+  transition: opacity 1s;
+  .loader__logo {
+    transition: opacity 0.6s;
+    transition-delay: 1s;
+  }
+}
+.fade-leave-active {
+  transition: opacity 1.2s;
+  transition-delay: 0.6s;
+  .loader__logo {
+    transition: opacity 0.6s, transform 1s cubic-bezier(0.33, 1, 0.68, 1);
+  }
+}
+.fade-enter {
+  opacity: 0;
+  .loader__logo {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+}
+.fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  .loader__logo {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+}
 </style>
