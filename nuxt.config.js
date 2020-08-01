ae6049bbbf50f7a8c66fd0ed87a25466af55e7cf
polunwu <polunwu@gmail.com>
2020-08-01 16:42:39 +0800

    Remove gh-pages cleanup & routerBase & npm export

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,6 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
-  router: {
-    base: '/polish-website-nuxt/',
-  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
