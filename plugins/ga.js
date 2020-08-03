1bbac2aaa7641d3a993caea9dd5923bef3c98110
polunwu <polunwu@gmail.com>
2020-08-04 00:17:43 +0800

    Add GTM pageview event on each page

--- plugins/ga.js
+++ plugins/ga.js
@@ -8,5 +8,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PR8JZD5');
 
-
+  app.router.afterEach((to, from) => {
+    dataLayer.push({ event: 'pageview' })
+  })
 }
